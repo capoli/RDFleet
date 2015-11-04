be305cce4e4f53105393065854e32a8242c972e4
capoli <olivier.cappelle@realdolmen.com>
2015-11-04 15:42:11 +0100

    style and representaion of data tweaks navabar customised

---
+++ webmvc-module/src/main/java/com/realdolmen/rdfleet/webmvc/controllers/rd/AjaxController.java
@@ -0,0 +1,26 @@
+package com.realdolmen.rdfleet.webmvc.controllers.rd;
+
+import com.realdolmen.rdfleet.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+/**
+ * Created by OCPAX79 on 4/11/2015.
+ */
+@Controller
+@RequestMapping("/api")
+public class AjaxController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    //TODO: .com wordt niet meegenomen
+    @RequestMapping(value = "/canOrderCar/{email}", method = RequestMethod.GET)
+    public @ResponseBody boolean getCanOrderCar(@PathVariable("email") String email) {
+        return employeeService.employeeCanOrderNewCar(String.format("%s.com", email));
+    }
+
+}
